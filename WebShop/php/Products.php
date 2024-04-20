81eb58f56929cc476897c4a7f726cd7cd68a330a
Kasper <kasperjnssen@gmail.com>
2024-04-21 00:26:55 +0200

    added Basket.png

---
+++ WebShop/php/Products.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kaspers Shop</title>
+    <link rel="stylesheet" href="../Design/Main.css">
+    <link rel="icon" href="../Images/LogoColour.png">
+</head>
+<?php
