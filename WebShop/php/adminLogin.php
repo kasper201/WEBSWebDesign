36ed06237599450a0292fc42743cbf60145e1fe6
Kasper <kasperjnssen@gmail.com>
2024-05-02 17:42:37 +0200

    Added login for admin (only for admin :])

---
+++ WebShop/php/adminLogin.php
@@ -0,0 +1,44 @@
+<?php
+// Start the session
+session_start();
+
+// Retrieve the username and password from the POST request
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$host = "localhost";
+$port = 3306;
+$database = "test";
+
+// Store the database credentials in the session
+$_SESSION['db_host'] = $host;
+$_SESSION['db_port'] = $port;
+$_SESSION['db_username'] = $username;
+$_SESSION['db_password'] = $password;
+$_SESSION['db_database'] = $database;
+
+// Check if $mysqli is set and if it's a valid mysqli object
+if (isset($mysqli) && $mysqli instanceof mysqli) {
+    // Kill the current connection
+    $mysqli->kill($mysqli->thread_id);
+    // Close the current connection
+    $mysqli->close();
+    // Unset the $mysqli object
+    unset($mysqli);
+}
+
+try {
+    // Create a new database connection
+    $mysqli = new mysqli($host, $username, $password, $database, $port);
+    header('Location: ./adminPages/admin.php');
+} catch (mysqli_sql_exception $e) { // Failed to connect to the database server
+    http_response_code(500); // Send a 500 status code
+    if($e->getCode() === 1045) {
+        echo '<script>alert("Invalid username or password")</script>';
+    } else {
+        echo '<script>alert("Failed to connect to MySQL")</script>';
+    }
+    //echo '<script>alert("Failed to connect to MySQL")</script>';
+    echo '<script>window.location.href = "./Login.php";</script>';
+    exit;
+}
---
+++ WebShop/php/adminPages/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kaspers Shop</title>
+    <link rel="stylesheet" href="../../Design/Admin.css">
+    <link rel="icon" href="../../Images/LogoColour.png">
+</head>
+<body>
+<div class="main">
+    <div class="content">
+        <h1>Admin</h1>
+        <div class="admin">
+            <a href="./adminPages/Products.php">
+                <button class="adminButton">Products</button>
+            </a>
+            <a href="./adminPages/Categories.php">
+                <button class="adminButton">Categories</button>
+            </a>
+            <a href="./adminPages/Orders.php">
+                <button class="adminButton">Orders</button>
+            </a>
+            <a href="../Main.php">
+                <button class="adminButton">back to home</button>
+            </a>
+        </div>
+    </div>
+</div>
+<script src="../../JS/Template.js"></script>
+</body>
+</html>
